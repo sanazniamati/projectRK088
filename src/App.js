e452c34d46a4d4309ea55bb99d1acaa2629ba525
sanazniamati <sanaz.niamati1991@gmail.com>
2022-07-16 17:59:36 +0430

    1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import {Stage,Layer,Line} from "react-konva";
+import { useState } from "react";
+import Konva from "konva";
+import { Stage, Layer, Line } from "react-konva";
 
-export default function App(){
+export default function App() {
+  const [blobs, setBlobs] = useState([10]);
+  const handelCreateBlob = () => {
+    setBlobs((prevBlobs) => [
+      ...prevBlobs,
+      {
+        x: (blobs.length - 1) * 150,
+        color: Konva.Util.getRandomColor(),
+      },
+    ]);
+    console.log(blobs);
+  };
   return (
+    <>
+      <button onClick={handelCreateBlob}> CreateBlob</button>
       <Stage width={window.innerWidth} height={window.innerHeight}>
         <Layer>
-          <Line
-              x={20}
-              y={200}
-              points={[145, 200,100,120,180,120]}
-              tension="0.5"
+          {blobs.map((blob, i) => (
+            <Line
+              key={i}
+              x={blob.x}
+              y={blob.y}
+              points={[50, 50, 150, 50, 100, 150]}
+              tension={0.5}
               closed
-              stroke="black"
-              fill="black"
+              fill={blob.color}
               draggable
-
-          />
+            />
+          ))}
         </Layer>
       </Stage>
+    </>
   );
-};
+}
